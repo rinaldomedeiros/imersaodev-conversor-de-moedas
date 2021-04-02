cedd7be0a4f211d7c02724d4e822ffe5f2276add
Rinaldo Medeiros <80865401+rinaldomedeiros@users.noreply.github.com>
2021-04-02 15:13:13 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var valorEmDolar = parseFloat(prompt("Qual o valor em dolar você quer converter?"))
+
+//var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+
+var valorEmReal = valorEmDolar * 5.5
+
+//var valorEmRealFixado = valorEmReal.toFixed(2)
+
+alert(valorEmReal.toFixed(2))
